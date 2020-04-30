d6eb3ab09ed50b77e4986e89adf29475e2832336
Joyce <wamungai29@gmail.com>
2020-04-30 22:34:54 +0300

    Add logic for movie categories.

--- app/views.py
+++ app/views.py
@@ -3,8 +3,11 @@ from app import app
 
 @app.route('/')
 def index():
+    popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movie_movie = get_movies('now_playing')
     title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movies, now_showing =now_showing_movie_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
