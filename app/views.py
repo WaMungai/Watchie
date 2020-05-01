1d2bd9ec46c5eb7116d4cdfbaee0322a929a8075
Joyce <wamungai29@gmail.com>
2020-05-01 15:54:35 +0300

    Create a search view function.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,url_for
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
 
 @app.route('/')
 def index():
@@ -15,3 +15,12 @@ def movie(id):
     movie = get_movie(id)
     title = f'{movie.title}'
     return render_template('movie.html',title=title ,movie=movie)
+
+@app.route('/search/<movie_name')
+def search(movie_name):
+    movie_name_list = movie_name.split("")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
+    
