7a2470e897963c380c277a3052893a3d152d7d81
Joyce <wamungai29@gmail.com>
2020-05-01 16:58:16 +0300

    Create the review class.

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ def index():
     title = 'Home - Welcome to the best Movie Review Website Online'
     
     search_movie = request.args.get('movie_query')
+    
     if search_movie:
         return redirect(url_for('search',movie_name=search_movie))
     else:
@@ -21,11 +22,11 @@ def movie(id):
     title = f'{movie.title}'
     return render_template('movie.html',title=title ,movie=movie)
 
-@app.route('/search/<movie_name')
+@app.route('/search/<movie_name>')
 def search(movie_name):
-    movie_name_list = movie_name.split("")
+    movie_name_list = movie_name.split(" ")
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies)
+    return render_template('search.html',movies = searched_movies )
     
