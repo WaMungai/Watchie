6dd815264ee86728642478231f8a52591accc93d
Joyce <wamungai29@gmail.com>
2020-05-01 11:20:46 +0300

    implement template inheritance.

--- app/request.py
+++ app/request.py
@@ -37,6 +37,6 @@ def process_results(movie_list):
         vote_count = movie_item.get('vote_count')
         
         if poster:
-            movie_object = movie(id,title,overview,poster,vote_average,vote_count)
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
             movie_results.append(movie_object)
     return movie_results
--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 @app.route('/')
 def index():
     popular_movies = get_movies('popular')
-    upcoming_movies = get_movies('upcoming')
-    now_showing_movie_movie = get_movies('now_playing')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movies, now_showing =now_showing_movie_movie)
+    return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movie, now_showing =now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
