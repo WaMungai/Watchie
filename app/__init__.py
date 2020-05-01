1cbb84d971090b4853d98a140a086359e27a2a14
Joyce <wamungai29@gmail.com>
2020-05-01 11:32:25 +0300

    Add and include navbar logic.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #Initialize application
 app = Flask(__name__,instance_relative_config = True)
@@ -8,5 +9,8 @@ app = Flask(__name__,instance_relative_config = True)
 #Set up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#Initialize Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
