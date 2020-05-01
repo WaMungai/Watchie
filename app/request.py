a10e2255701cee95d8177fd5a6f84e10c623d477
Joyce <wamungai29@gmail.com>
2020-05-01 12:31:35 +0300

    Add logic for links and static files.

--- app/request.py
+++ app/request.py
@@ -27,7 +27,7 @@ def get_movies(category):
     return movie_results
 
 def get_movie(id):
-    get_movie_detials_url = base_url.format(id,api_key)
+    get_movie_details_url = base_url.format(id,api_key)
     
     with urllib.request.urlopen(get_movie_details_url) as url:
         movie_details_data = url.read()
@@ -40,7 +40,7 @@ def get_movie(id):
             overview = movie_details_response.get('overview')
             poster = movie_details_response.get('poster_path')
             vote_average = movie_details_response.get('vote_average')
-            movie_object = movie_details_response.get('vote_count')
+            vote_count = movie_details_response.get('vote_count')
             
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
             
--- app/views.py
+++ app/views.py
@@ -10,8 +10,8 @@ def index():
     title = 'Home - Welcome to the best Movie Review Website Online'
     return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movie, now_showing =now_showing_movie)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
     movie = get_movie(id)
     title = f'{movie.title}'
-    return render_template('movie.html',movie=movie)
+    return render_template('movie.html',title=title ,movie=movie)
