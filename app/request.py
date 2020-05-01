bcfddedf1612ff44609ae62ebac0b1ee2c801eeb
Joyce <wamungai29@gmail.com>
2020-05-01 11:48:57 +0300

    Create function for gettinf movi details for a single movie.

--- app/request.py
+++ app/request.py
@@ -26,6 +26,26 @@ def get_movies(category):
             
     return movie_results
 
+def get_movie(id):
+    get_movie_detials_url = base_url.format(id,api_key)
+    
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+        
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            movie_object = movie_details_response.get('vote_count')
+            
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            
+        return movie_object
+    
 def process_results(movie_list):
     movie_results = []
     for movie_item in movie_list:
