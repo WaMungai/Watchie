23ea04eb63e1393c629efc8dccad30bc88e5a6df
Joyce <wamungai29@gmail.com>
2020-05-02 04:03:06 +0300

    Fix bug.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import CERT_REQUIRED
+from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
     title = StringField('Review title,',validators=[Required()])
